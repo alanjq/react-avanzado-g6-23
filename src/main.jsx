e37277fffb625654abd5b05137c19d09d2f492c1
Alan Jimenez <alanjq@outlook.com>
2023-05-17 21:28:12 -0500

    Agregar ruta para el HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from './HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomePage />
+    {/* Creamos el Router para cargar la página de HomePage */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
