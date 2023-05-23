df4f427cfd28e4ba1858de887edd56a08a685d25
Alan Jimenez <alanjq@outlook.com>
2023-05-22 22:51:39 -0500

    Texto para probar fixed header

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,51 @@
 import React from "react";
 import {Button} from '@mui/material'
 import Welcome from "./Landing/Welcome";
-import NavBar from '../components/Navbar'
+
 
 export const LandingPage = () => {
     
     return (<div>
-        <NavBar />
         <Welcome />
-        <Button variant="text">Text</Button>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
+        <p><Button variant="text">Text</Button></p>
     </div>)
 }
 
