d0cb5b7d6bce4ffaaada54e43becbb469f6747b6
AlejandroLeon23 <lic.alejandroleonlomeli@gmail.com>
2023-05-22 20:42:33 -0700

    Commit para agregar el NavBar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {Button} from '@mui/material'
+import NavBar from '../components/Navbar'
 
 export const LandingPage = () => {
     
     return (<div>Landing page shopping
         <Button variant="text">Text</Button>
+        <NavBar></NavBar>
     </div>)
 }
 
