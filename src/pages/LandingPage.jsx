8db017bc85ef3d841e3ef6fc1678bd7ab876b263
Alan Jimenez <alanjq@outlook.com>
2023-05-22 21:36:56 -0500

    Botón de ejemplo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {Button} from '@mui/material'
 
 export const LandingPage = () => {
     
-    return (<div>Landing page shopping</div>)
+    return (<div>Landing page shopping
+        <Button variant="text">Text</Button>
+    </div>)
 }
 
 export default LandingPage
