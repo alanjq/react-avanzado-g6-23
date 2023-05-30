74c81dc3eaf28d766aa55171e9654872664132d9
C3M3Z4C4 <cmeza@mezadigital.com>
2023-05-29 19:31:30 -0600

    Modificaciones en context

--- src/SiteRouter.jsx
+++ src/SiteRouter.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import {useState} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from './pages/LandingPage'
 import CartPage from './pages/CartPage'
@@ -9,28 +9,35 @@ import SiteLayout from "./SiteLayout";
 import { CartContext } from "./context/CartContext";
 
 
-export const SiteRouter = () => (
-    <CartContext.Provider value={{items:[
-        {
-            articleName: "magazine",
-            price: 15,
-            stock: 1,
-            picture: 'none',
-        }
-    ]}}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<SiteLayout />}>
-                    <Route index element={<LandingPage />} />
-                    <Route path="shopping-cart" element={<CartPage />} />
-                    {/* TODO: Definir variable para producto */}
-                    <Route path="product" element={<ProductPage />} />
-                    <Route path="contact" element={<ContactPage />} />
-                    <Route path="checkout" element={<CheckoutPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </CartContext.Provider>
-)
+export const SiteRouter = () => {
+    const [listaArticulos, setListaArticulos] = useState([])
+
+
+    // items:[
+    //     {
+    //         articleName: "magazine",
+    //         price: 15,
+    //         stock: 1,
+    //         picture: 'none',
+    //     }
+    // ]
+
+    return(   
+        <CartContext.Provider value={{listaArticulos, setListaArticulos}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SiteLayout />}>
+                        <Route index element={<LandingPage />} />
+                        <Route path="shopping-cart" element={<CartPage />} />
+                        {/* TODO: Definir variable para producto */}
+                        <Route path="product" element={<ProductPage />} />
+                        <Route path="contact" element={<ContactPage />} />
+                        <Route path="checkout" element={<CheckoutPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </CartContext.Provider>
+    ) 
+}
 
 export default SiteRouter
