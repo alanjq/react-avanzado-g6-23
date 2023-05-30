ad77249654b70b56b1a96be6fd07a5b841e5d519
C3M3Z4C4 <cmeza@mezadigital.com>
2023-05-29 19:57:08 -0600

    Agregando contectos de contador, agregar productos y agregar detalles de producto

---
+++ src/Context/index.jsx
@@ -0,0 +1,39 @@
+
+import { createContext, useState } from 'react'
+
+
+export const ShoppingCartContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const ShoppingCartProvider = ({children}) => {
+// Shopping cart: Increment quantity and 
+  const [count, setCount] = useState(0);
+  const [cartProducts, setCartProducts] = useState([]);
+  
+  //Open Close Product
+  const [isProductDetailOpen, setIsProductOpen] = useState(false);
+  const openProductDetail = () => setIsProductOpen(true)
+  const closeProductDetail = () => setIsProductOpen(false)
+
+  //Product Detail Show Product
+  const [productToShow, setProductToShow] = useState({});
+
+
+  
+  return(
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts, 
+      setCartProducts
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+    
+  )
+}
