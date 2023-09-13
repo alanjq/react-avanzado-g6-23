404ec9dcf72b1acae91baf279b82e3681e652e8c
Alan Jimenez <alanjq@outlook.com>
2023-09-13 17:48:09 -0500

    React sesion 2

---
+++ sesion2/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export function HomePage() {
+    return (
+        <main>
+            <h1>Bienvenidos</h1>
+            <p>Esta es mi página hecha con React</p>
+        </main>
+    )
+}
+export default HomePage
